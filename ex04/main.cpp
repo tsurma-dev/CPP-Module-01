b6914c1f35d09673e323b0eb14a21735b1943d4c
tsurma <tsurma@student.42berlin.de>
2024-08-03 15:15:50 +0200

    ex04 done.

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tsurma <tsurma@student.42.fr>              +#+  +:+       +#+        */
+/*   By: tsurma <tsurma@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 16:06:31 by tsurma            #+#    #+#             */
-/*   Updated: 2024/08/02 17:39:54 by tsurma           ###   ########.fr       */
+/*   Updated: 2024/08/03 15:14:30 by tsurma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,28 @@
 #include <fstream>
 
 int main(int argc, char **argv) {
-	std::ifstream	infile;
-	std::ifstream	outfile;
-
 	if (argc != 4) {
 		std::cout << "<filename> <to replace> <replacer>" << std::endl;
 		return (EINVAL);
 	}
-	std::string	tofind = argv[2];
-	std::string	replace = argv[3];
+	std::ifstream	infile;
+	std::ofstream	outfile("outfile");
+	std::string		tofind = argv[2];
+	std::string		replace = argv[3];
+	std::string::size_type pos = 0;
+
 	infile.open(argv[1]);
 	if (!infile.is_open()) {
 		std::cout << "Invalid file" << std::endl;
 		return (EINVAL);
 	}
-	std::string::size_type pos = 0;
 	std::string content((std::istreambuf_iterator<char>(infile)), std::istreambuf_iterator<char>());
 	while ((pos = content.find(tofind, pos)) != std::string::npos) {
 		content.erase(pos, tofind.length());
 		content.insert(pos, replace);
 		pos += replace.length();
 	}
-	std::cout << content << std::endl;
+	infile.close();
+	outfile << content << std::endl;
+	return (0);
 }
